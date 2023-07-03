aeeb62bc14efaf01848c5a16815b9cf577f0d190
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-07-03 15:33:54 +0100

    Add Paris to JS time function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,20 @@
 function updateTime() {
   let londonElement = document.querySelector("#london");
-  let londonDateElement = document.querySelector(".date");
-  let londonTimeElement = document.querySelector(".time");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
   let londonTime = moment().tz("Europe/London");
 
   londonDateElement.innerHTML = londonTime.format("Do MMMM YYYY");
   londonTimeElement.innerHTML = londonTime.format(
+    "hh:mm:ss [<small>]A [</small>]"
+  );
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
     "hh:mm:ss [<small>]A [</small>]"
   );
 }
