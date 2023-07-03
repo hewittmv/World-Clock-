03e6d5b6dd960a61dfe66912c7c38fb9865ea442
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-07-03 20:26:40 +0100

    amend hierarchy of code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -19,10 +19,10 @@ function updateTime() {
   );
 }
 function updateCity(event) {
+  let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  let cityTimeZone = event.target.value;
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
